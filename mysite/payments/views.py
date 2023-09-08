b3d689ed082943bbeb586549f0b8722bec2f5755
Sergey Gavrilov <one6sergey@gmail.com>
2023-09-08 17:14:06 +0300

    add payment validation

--- mysite/payments/views.py
+++ mysite/payments/views.py
@@ -3,19 +3,26 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from orders.models import Order
 from .models import Payment
+from .serializers import PaymentSerializer
+from orders.models import Basket
 # Create your views here.
 
 
 class PaymentView(APIView):
-    # добавить валидацию и удаление корзины для пользователя
     def post(self, request, id):
         order = Order.objects.get(id=id)
         data = request.data
-        print(data)
-        Payment.objects.create(order=order,
-                               card_number=data['number'],
-                               name=data['name'],
-                               month=data['month'],
-                               year=data['year'],
-                               code=data['code'])
-        return Response(status=200)
+        serializer = PaymentSerializer(data=data)
+        if serializer.is_valid():
+            Payment.objects.create(order=order,
+                                card_number=data['number'],
+                                name=data['name'],
+                                month=data['month'],
+                                year=data['year'],
+                                code=data['code'])
+            basket = Basket.objects.get(user=request.user)
+            basket.delete()
+            return Response(status=200)
+        else:
+            print(serializer.errors)
+            return Response(serializer.errors, status=400)
